58a23c5acdd1dbc04c5367eac784fce11755e155
Njål Nordmark <njaal.nordmark@gmail.com>
2023-10-04 18:54:26 +0000

    Add basic Spring Boot application

---
+++ src/main/java/xyz/ascii/unleashpoc/service/a/Application.java
@@ -0,0 +1,11 @@
+package xyz.ascii.unleashpoc.service.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
