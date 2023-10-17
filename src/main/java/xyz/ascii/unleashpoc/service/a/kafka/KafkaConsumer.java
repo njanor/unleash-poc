2fd0c701145124b74bb0b0fd75da42a93ef0cfbe
Njål Nordmark <njaal.nordmark@gmail.com>
2023-10-17 04:43:57 +0000

    Add basic toggle of Kafka consumer

--- src/main/java/xyz/ascii/unleashpoc/service/a/kafka/KafkaConsumer.java
+++ src/main/java/xyz/ascii/unleashpoc/service/a/kafka/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package xyz.ascii.unleashpoc.service.a.kafka;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Service;
 import io.getunleash.Unleash;
 import jakarta.annotation.PostConstruct;
 import reactor.kafka.receiver.KafkaReceiver;
-import reactor.util.retry.Retry;
+import reactor.kafka.receiver.ReceiverRecord;
+import reactor.util.retry.RetrySpec;
 
 @Service
 public class KafkaConsumer {
@@ -22,7 +24,15 @@ public class KafkaConsumer {
     @PostConstruct
     void consumeKafkaTopic() {
         kafkaReceiver.receive()
-        //.retryWhen(Retry.backoff(0, null))
+            .<ReceiverRecord<Integer, String>>handle((item, sink) -> {
+                if (unleash.isEnabled("kafka-toggle")) {
+                    sink.next(item);
+                } else {
+                    sink.error(new RuntimeException("Not subscribed"));
+                }
+            })
+            .doOnError(error -> System.out.println("'kafka-toggle' not enabled yet"))
+            .retryWhen(RetrySpec.fixedDelay(1000, Duration.ofSeconds(10)))
             .doOnNext(record -> System.out.printf("%s: Processing record %s\nValue %s", LocalDateTime.now(), record.offset(), record.value()))
             .doOnNext(record -> record.receiverOffset().acknowledge())
             .count()
